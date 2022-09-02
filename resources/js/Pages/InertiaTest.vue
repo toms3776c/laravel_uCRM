d9a23b9c48cdd2dc75b7919d78662c32e1418cc8
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-09-02 09:25:52 +0900

    Inertia Test

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+</script>
+
+<template>
+Inertiaテストです。<br/>
+<a href="/">aタグ経由です</a><br/>
+<Link href="/">Link経由です</Link>
+<Link :href="route('inertia.index')">名前付きルートの確認です</Link><br/>
+<Link :href="route('inertia.show', {id: 1})">ルートパラメータの確認です</Link>
+</template>
