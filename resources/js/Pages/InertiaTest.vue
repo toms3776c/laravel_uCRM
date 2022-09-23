3904aa8c0716c052f0070faf3ffef0f4412456a7
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-09-23 16:53:43 +0900

    add store

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue'
 
+const newTitle = ref('')
+const newContent = ref('')
 </script>
 
 <template>
@@ -9,4 +12,14 @@ Inertiaテストです。<br/>
 <Link href="/">Link経由です</Link>
 <Link :href="route('inertia.index')">名前付きルートの確認です</Link><br/>
 <Link :href="route('inertia.show', {id: 1})">ルートパラメータの確認です</Link>
+
+<div class="mb-8"></div>
+
+<input type="text" name="newTitle" v-model="newTitle"><br>
+<input type="text" name="newContent" v-model="newContent"><br>
+<Link as="button" method="post" :href="route('inertia.store')" :data="{
+    title: newTitle,
+    content: newContent
+}">DB保存テスト</Link>
+
 </template>
